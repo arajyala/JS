5c35f969e3c5d23142beacb750324829cabbb910
arajyala <rajyalaxmianupati1999@gmail.com>
2024-01-02 12:45:17 +0530

    Assignments added

---
+++ primeNumbers1To100.js
@@ -0,0 +1,10 @@
+//4)write a programme to print prime numbers 1 to 100(using loops).
+
+
+for(var i = 2; i<=100 ; i++)
+{
+    if(i%2===1)
+    {
+        console.log(" " +i);
+    }
+}
---
+++ squaresOfFirst10.js
@@ -0,0 +1,8 @@
+//5. program squares for first 10 number using for loop
+
+var sqrt;
+for(var i = 1; i <=10 ; i++)
+{
+    sqrt = i*i;
+    console.log(" "+sqrt);
+}
