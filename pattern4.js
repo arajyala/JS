02cb81eb92e84a0ff737cf6dc0264c72bfc44db3
arajyala <rajyalaxmianupati1999@gmail.com>
2024-01-02 12:56:16 +0530

    assignments added

---
+++ pattern4.js
@@ -0,0 +1,17 @@
+/* print the below
+
+       ****
+       ***
+       **
+       *
+*/
+for(var i=4; i>=1; i--)
+{
+       var line ="";
+       for(var j=1;j<=i;j++)
+       {
+              line += "*";
+       }
+
+       console.log(line);
+}
