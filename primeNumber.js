29284c3d3c8768ea1b2bedc1f5e7ce57a1337077
arajyala <rajyalaxmianupati1999@gmail.com>
2024-01-02 12:39:03 +0530

    assignments added

---
+++ primeNumber.js
@@ -0,0 +1,12 @@
+//2.Write a logic for prime Number
+//the number which is divisible by 1 and itself ex:2,3,5,7,11,13,17,19,23....
+
+var num=83;
+if(num>1 && num%2 ==1)
+{
+    console.log("The given Number is prime");
+}
+else
+{
+    console.log("The given Number is Not a prime");
+}
